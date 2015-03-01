0f7b7505788e78d2f72a9dcf08a577892004b6f6
Dave Descheneaux <davedesch@gmail.com>
2015-02-28 23:50:11 -0800

    added all new features with ajax and new css

---
+++ public/js/blog.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+  console.log("ready!");
+  headerIntercept();
+
+})
+
+
+function headerIntercept(){
+  $('.header').on('click', function(event){
+    event.preventDefault();
+    console.log("CLICKED!");
+    $.ajax({
+      url: 'squirrelbingo.html',
+      type: 'GET',
+      dataType: 'html',
+    })
+    .done(function(a) {
+      console.log("success");
+      console.log(a);
+    })
+    .fail(function() {
+      console.log("error");
+    })
+    .always(function() {
+      console.log("complete");
+    });
+
+  })
+}
